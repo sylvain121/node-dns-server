a917bb41249ecf61817f7631b32322334b7fcf44
Sylvain MATHIEU <sylvain.mathieu@orange.com>
2019-09-10 16:32:06 +0200

    dns serveur with nodejs forward dns request to host dns server

---
+++ index.js
@@ -0,0 +1,20 @@
+const dns = require('dns');
+var named = require('./lib/index');
+var server = named.createServer();
+var ttl = 300;
+const port = 3000
+server.listen(port, '127.0.0.1', function () {
+    console.log('DNS server started on port '+port);
+});
+
+server.on('query', function (query) {
+    var domain = query.name();
+    console.log('DNS Query: %s', domain);
+    dns.lookup(domain, null, (err, address, family) => {
+        console.log('address: %j family: IPv%s', address, family);
+        var target = new named.ARecord(address);
+        query.addAnswer(domain, target, ttl);
+        server.send(query);
+    });
+
+});
